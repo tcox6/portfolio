4bf2b6e5d8afc4947838832065b9cad5931cee14
Tim <tcox6@utas.edu.au>
2025-11-05 10:58:03 +1100

    first commit

---
+++ scripts/game-of-life.js
@@ -0,0 +1,168 @@
+const canvas = document.getElementById('gameOfLifeCanvas');
+const ctx = canvas.getContext('2d');
+
+// define parameters
+const CELL_SIZE = 20; // px
+const UPDATE_FREQ = 1; // seconds
+
+// declare the game board
+var board;
+
+/**
+ * Handles canvas resize events (which occur when the webpage is resized).
+ */
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = document.documentElement.scrollHeight;
+
+    // recreate the game of life board
+    const NUM_COLUMNS = canvas.width / CELL_SIZE;
+    const NUM_ROWS = canvas.height / CELL_SIZE;
+    if (typeof board == 'undefined') {
+        initBoard(NUM_COLUMNS, NUM_ROWS);
+    } else {
+        resizeBoard(NUM_COLUMNS, NUM_ROWS);
+    }
+    renderBoard();
+}
+
+/**
+ * Initialises the board array with a starting seed.
+ * @param {Number} columns The number of columns in the board
+ * @param {Number} rows The number of rows in the board
+ */
+function initBoard(columns, rows) {
+    // board has not been initialised, need to create it
+    board = [];
+    for (let y = 0; y < rows; y++) {
+        board[y] = [];
+        for (let x = 0; x < columns; x++) {
+            board[y][x] = 0; // dead
+        }
+    }
+
+    // create starting pattern
+    board[Math.floor(rows / 2)][Math.floor(columns / 2) - 1] = 1;
+    board[Math.floor(rows / 2)][Math.floor(columns / 2) - 2] = 1;
+    board[Math.floor(rows / 2)][Math.floor(columns / 2) - 3] = 1;
+    board[Math.floor(rows / 2) - 1][Math.floor(columns / 2) - 3] = 1;
+    board[Math.floor(rows / 2) + 1][Math.floor(columns / 2) - 2] = 1;
+    board[Math.floor(rows / 2) - 1][Math.floor(columns / 2) + 1] = 1;
+    board[Math.floor(rows / 2) - 1][Math.floor(columns / 2) + 2] = 1;
+    board[Math.floor(rows / 2) - 1][Math.floor(columns / 2) + 3] = 1;
+    board[Math.floor(rows / 2)][Math.floor(columns / 2) + 2] = 1;
+}
+
+/**
+ * Resizes the board, keeping the existing board state.
+ * @param {Number} columns The number of columns in the new board
+ * @param {Number} rows The number of rows in the new board
+ */
+function resizeBoard(columns, rows) {
+    let newBoard = []
+    for (let y = 0; y < rows; y++) {
+        newBoard[y] = []
+        for (let x = 0; x < columns; x++) {
+            if (y < board.length && x < board[y].length) {
+                newBoard[y][x] = board[y][x];
+            } else {
+                newBoard[y][x] = 0; // dead
+            }
+        }
+    }
+
+    board = newBoard
+}
+
+/**
+ * Updates and renders game of life board.
+ */
+function step() {
+    updateBoard();
+    renderBoard();
+}
+
+/**
+ * Updates the game of life board state.
+ */
+function updateBoard() {
+    // create a copy of the previous board state
+    let newBoard = [];
+    for (let y = 0; y < board.length; y++) {
+        newBoard[y] = board[y].slice();
+    }
+
+    // iterate through the previous board state and update `newBoard`
+    for (let y = 0; y < board.length; y++) {
+        for (let x = 0; x < board[y].length; x++) {
+            let aliveNeighbours = getAliveNeighbours(x, y);
+            if (board[y][x] == 1 && aliveNeighbours < 2) {
+                newBoard[y][x] = 0; // dies
+            } else if (board[y][x] == 1 && (aliveNeighbours == 2 || aliveNeighbours == 3)) {
+                // this is actually redundant
+                newBoard[y][x] = 1;
+            } else if (board[y][x] == 1 && aliveNeighbours > 3) { 
+                newBoard[y][x] = 0; // dies
+            } else if (board[y][x] == 0 && aliveNeighbours == 3) { 
+                newBoard[y][x] = 1; // reproduction
+            }
+        }
+    }
+
+    board = newBoard
+}
+
+/**
+ * Helper function of updateBoard.
+ * Calculates the number of alive neighbours for a given cell in `board`.
+ * @param {Number} cellX The x-value of the cell.
+ * @param {Number} cellY The y-value of the cell.
+ * @returns {Number} The number of alive neighbours.
+ */
+function getAliveNeighbours(cellX, cellY) {
+    let aliveNeighbours = 0;
+    for (const dx of [-1, 0, 1]) {
+        for (const dy of [-1, 0, 1]) {
+            let x = cellX + dx;
+            let y = cellY + dy;
+
+            // range checking
+            if (y > 0 && y < board.length && x > 0 && x < board[y].length) {
+                // don't allow dx==dy==0
+                if (!(dx == 0 && dy == 0)) {
+                    // check whether cell is alive
+                    if (board[y][x] == 1) {
+                        aliveNeighbours++;
+                    }
+                }
+            }
+        }
+    }
+
+    return aliveNeighbours;
+}
+
+/**
+ * Renders game of life board on canvas.
+ */
+function renderBoard() {
+    // check if board has been initialised
+    if (typeof board !== 'undefined') {
+        // clear canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = "red"; // TODO: change!
+
+        for (let y = 0; y < board.length; y++) {
+            for (let x = 0; x < board[y].length; x++) {
+                if (board[y][x] == 1) {
+                    ctx.fillRect(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                }
+            }
+        }
+    }
+}
+
+window.addEventListener('resize', resizeCanvas);
+window.addEventListener('load', resizeCanvas);
+setTimeout(() => setInterval(step, UPDATE_FREQ * 1000), 1000);
