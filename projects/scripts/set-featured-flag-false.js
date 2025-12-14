05041487a1b6f0703f9dd570bb5249401a75ca0d
Tim Cox <tim20home@outlook.com>
2025-12-14 12:32:43 +1100

    Refactored some of the styling for project lists, created thumbnails for the 'createt' and 'RC motorbike' projects, improved consistency between project thumbnail file names. Also added a 'featured' flag to the projects JSON list for each project, added global flags specifying whet her only featured projects should be shown, and adapted 'generate-project-html.js' to recognise this flag.

---
+++ projects/scripts/set-featured-flag-false.js
@@ -0,0 +1,4 @@
+/**
+ * Declares a global flag specifying that the `generate-project-html.js' script should show all projects, regardless of whether they are marked as featured.
+ */
+var featuredFlag = false;
---
+++ projects/scripts/set-featured-flag-true.js
@@ -0,0 +1,4 @@
+/**
+ * Declares a global flag specifying that the `generate-project-html.js' script should only show projects which are marked as 'featured'.
+ */
+var featuredFlag = true;
