f044273e7f017fda7ce8d3e00adda62384462179
Tim Cox <tim20home@outlook.com>
2026-02-08 14:07:21 +1100

    Added a brief write-up for DSRS project.
    Updated write-up for generalised coordinate learning.
    
    Modified footer implementation so that it stretches to the bottom of the page when the page contents are shorter than the viewport height.

--- footer/load-footer.js
+++ footer/load-footer.js
@@ -2,4 +2,10 @@ fetch("/footer/footer.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("footer").innerHTML = data;
+
+        // make footer extend to the end of the page
+        const footerEmbedded = document.getElementById("footerEmbedded");
+        if (footerEmbedded.offsetTop + footerEmbedded.offsetHeight < document.body.offsetHeight) {
+            footerEmbedded.style.height = String(document.body.offsetHeight - footerEmbedded.offsetTop) + "px";
+        }
     });
