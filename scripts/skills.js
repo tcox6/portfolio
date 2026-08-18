a93bf05ccecb631425e1f04d4c412aae1abb97ec
Tim Cox <tcox6@utas.edu.au>
2026-08-18 22:06:11 +1000

    Finished skill bubbles feature.
    Fixed bug with path generation, finalised client-side rendering of bubbles, and added an event listener for the introduction div being resized.
    
    Still need to add a mechanism for re-spacing path position indices when the introduction div is resized.

--- scripts/skills.js
+++ scripts/skills.js
@@ -8,22 +8,31 @@ COLOURS = [
 // Container for the bubbles
 const bubbleContainer = document.getElementById("floatingStuff");
 
+// The thing that we want the bubbles to move around
+const introContainer = document.getElementsByClassName("introduction")[0];
+
+// Path that the bubbles follow
+// Populated by makeBubblesPath
+let bubblesPath;
+
+// Don't want two consecutive colours that are the same
+let prev_colour = "";
+
 /**
  * Constructs a path around the border of the 'introduction' div that the bubbles
     move around. Path consists of a sequence of (x,y) points and features rounded
     edges. The distance between all points is the same, as these are used as keyframes
     for moving the bubbles around the path.
  */
-function makebubblesPathIntermediate() {
-    const introContainer = document.getElementsByClassName("introduction")[0];
+function makeBubblesPath() {
     const rect = introContainer.getBoundingClientRect();
     // Path can be constructed by just considering the quarter-circles in the corners
     // and filling-in the straight-edge lines with a sequence of points.
     // This will not produce perfectly-distributed points for the quarter-circles, but 
     // it is close enough for small values of r.
-    const r = 24;  // Radius, px
-    const N = 12;  // Number of points in quarter-circle
-    const delta = Math.PI / 2 / (N - 1);  // Increment in angle for each point (rads)
+    const r = 96;  // Radius, px
+    const N = 100;  // Number of points in quarter-circle
+    const delta = (Math.PI / 2) / (N - 1);  // Increment in angle for each point (rads)
     // Calculate the position of the centers of the corner circles
     const CENTERS = [
         {x: rect.right - r, y: rect.top + r}, 
@@ -42,7 +51,7 @@ function makebubblesPathIntermediate() {
         for (let j = 0; j < N; j++) {
             bubblesPathIntermediate[2*i].push({
                 x: CENTERS[i].x + r * Math.cos(theta),
-                y: CENTERS[i].y + r * Math.sin(theta),
+                y: CENTERS[i].y - r * Math.sin(theta),
             });
             theta += delta;
         }
@@ -55,7 +64,7 @@ function makebubblesPathIntermediate() {
         let d_c = Math.sqrt((p_c0.x-p_c1.x)**2 + (p_c0.y-p_c1.y)**2);
         // Points for the linear lines
         let p_l0 = {x:CENTERS[i].x + r * Math.cos(theta), y:CENTERS[i].y - r * Math.sin(theta)};
-        let p_l1 = {x:CENTERS[(i+1)%2].x + r * Math.cos(theta), y:CENTERS[(i+1)%2].y - r * Math.sin(theta)};
+        let p_l1 = {x:CENTERS[(i+1)%4].x + r * Math.cos(theta), y:CENTERS[(i+1)%4].y - r * Math.sin(theta)};
         let d_l = Math.sqrt((p_l0.x-p_l1.x)**2 + (p_l0.y-p_l1.y)**2);  // Length of the line segment
         let n_new_pts = Math.round(d_l / d_c);  // Actual number of new points will be 1 less
         let new_p = p_l0;
@@ -83,83 +92,103 @@ function makebubblesPathIntermediate() {
 
     return bubblesPath;
 }
-let bubblesPath = makebubblesPathIntermediate();
-window.addEventListener('resize', (event) => {
-    bubblesPath = makebubblesPathIntermediate();
-});
+// Construct initial path
+bubblesPath = makeBubblesPath();
 
-// // Get JSON and make the 'bubbles'
-// fetch('/skills.json')
-//     .then(response => {
-//         return response.json();
-//     })
-//     .then(data => {
-//         const bubbles = data;
+// Get JSON and make the 'bubbles'
+fetch('/skills.json')
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const bubbles = data;
 
-//         for (let i = 0; i < bubbles.Skills.length; i++) {
+        // Random index order
+        let order = [];
+        for (let i = 0; i < bubbles.Skills.length; i++) {
+            order[i] = i;
+        }
+        order.sort(() => Math.random() - 0.5);
 
-//         }
-
-//         setInterval(() => {
-//             // Select text randomly
-//             let valid = false;
-//             let text;
-//             while (!valid) {
-//                 text = bubbles.Skills[Math.floor(Math.random() * bubbles.Skills.length)];
-//                 valid = text != prev_text;
-//             }
-//             prev_text = text;
-//             makeBubble(text, timEdges);
-//         }, 1500)
-//     })
+        // Spawn bubbles
+        let spawn_idx = 0;  // The path node to spawn at
+        for (let i = 0; i < bubbles.Skills.length; i++) {
+            makeBubble(bubbles.Skills[order[i]], spawn_idx);
+            // TODO: will introduce minor rounding errors
+            spawn_idx += Math.round(bubblesPath.length / bubbles.Skills.length);
+        }
+    })
 
 
-// function makeBubble(text, timEdges) {
-//     const bubble = document.createElement("span");
-//     bubble.innerHTML = text;
-//     bubble.classList.add("bubble");
+function makeBubble(text, spawn_idx) {
+    const bubble = document.createElement("span");
+    bubble.innerHTML = text;
+    bubble.classList.add("bubble");
 
-//     // Need to append to parent here so can get rendered width
-//     bubble.style.visibility = "hidden";
-//     bubbleContainer.appendChild(bubble);
+    bubble.style.visibility = "hidden";
+    bubbleContainer.appendChild(bubble);
 
-//     // Random pos along border of 'Tim' heading
-//     // Choose the edge
-//     let valid = false;
-//     let edge_idx;
-//     while (!valid) {
-//         edge_idx = Math.floor(Math.random() * 4);
-//         valid = edge_idx != prev_edge_idx;
-//     }
-//     prev_edge_idx = edge_idx;
-//     let edge = timEdges[edge_idx];
-//     // Randomly get an extent along this edge
-//     let extent = Math.random();
-//     // Now just interpolate between the two points
-//     let x = edge.x0 + (edge.x1 - edge.x0) * extent;
-//     let y = edge.y0 + (edge.y1 - edge.y0) * extent;
-//     // Add x offset if left/right edge
-//     if (edge_idx == 1 || edge_idx == 2) {
-//         x -= bubble.offsetWidth / 2;
-//     }
+    let bubbleIdx = spawn_idx;
+    let x = bubblesPath[bubbleIdx].x;
+    let y = bubblesPath[bubbleIdx].y;
+    bubble.style.left = `${x - bubble.offsetWidth / 2}px`;
+    bubble.style.top = `${y - bubble.offsetHeight / 2}px`;
 
-//     bubble.style.left = `${x}px`;
-//     bubble.style.top = `${y}px`;
+    let rotateDegrees = Math.atan2(
+        bubblesPath[(bubbleIdx + 1)%bubblesPath.length].y - bubblesPath[bubbleIdx].y,
+        bubblesPath[(bubbleIdx + 1)%bubblesPath.length].x - bubblesPath[bubbleIdx].x,
+    ) * 180 / Math.PI + 180;
+    bubble.style.transform = `rotate(${rotateDegrees}deg)`;
 
-//     // Set random background colour
-//     valid = false;
-//     let colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
-//     while (!valid) {
-//         colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
-//         valid = colour != prev_colour;
-//     }
-//     prev_colour = colour;
-//     bubble.style.backgroundColor = colour;
+    // Set random background colour
+    valid = false;
+    let colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
+    while (!valid) {
+        colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
+        valid = colour != prev_colour;
+    }
+    prev_colour = colour;
+    bubble.style.backgroundColor = colour;
 
-//     bubble.style.visibility = "visible";
+    bubble.style.visibility = "visible";
 
-//     // Need to remove the bubble after some time
-//     setTimeout(() => {
-//         bubbleContainer.removeChild(bubble);
-//     }, 4000);
+    // Update position every 0.1 seconds
+    setInterval(() => {
+        bubbleIdx = (1 + bubbleIdx) % bubblesPath.length;
+        x = bubblesPath[bubbleIdx].x;
+        y= bubblesPath[bubbleIdx].y;
+        bubble.style.left = `${x - bubble.offsetWidth / 2}px`;
+        bubble.style.top = `${y - bubble.offsetHeight / 2}px`;
+
+        
+        let rotateDegrees = Math.atan2(
+            bubblesPath[(bubbleIdx + 1)%bubblesPath.length].y - bubblesPath[bubbleIdx].y,
+            bubblesPath[(bubbleIdx + 1)%bubblesPath.length].x - bubblesPath[bubbleIdx].x,
+        ) * 180 / Math.PI + 180;
+        bubble.style.transform = `rotate(${rotateDegrees}deg)`;
+    }, 10);
+}
+
+// Debugging
+// function resizeBubblePath() {
+//     bubblesPath = makeBubblesPath();
+    // bubbleContainer.replaceChildren()
+    // for (let i = 0; i<bubblesPath.length; i++) {
+    //     const bubble = document.createElement("span");
+    //     bubble.classList.add("bubble");
+    //     bubble.style.width = "8px";
+    //     bubble.style.height = "8px";
+    //     bubble.style.borderRadius = "4px";
+    //     bubble.style.backgroundColor = "red";
+    //     bubble.style.left = `${bubblesPath[i].x}px`;
+    //     bubble.style.top = `${bubblesPath[i].y}px`;
+    //     bubbleContainer.appendChild(bubble);
+    // }
 // }
+window.addEventListener('resize', (event) => {
+    // TODO: adjust position indices
+    bubblesPath = makeBubblesPath();
+});
+// Also need to recreate when introduction container resized
+//https://stackoverflow.com/a/49475832
+new ResizeObserver(() => {bubblesPath = makeBubblesPath();}).observe(introContainer);
